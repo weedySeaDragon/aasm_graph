a9656d93c055a33c93ee66ca53befbfa51565638
Ashley Engelund (weedySeaDragon @ github) <ashley@ashleycaroline.com>
2016-12-18 23:26:29 -0800

    added --class option; remove -I and -R options; catch exceptions; add tests for the classes, class files

--- lib/aasm/graph/cli.rb
+++ lib/aasm/graph/cli.rb
@@ -1,36 +1,86 @@
 module AASM
   module Graph
+
+    # TODO option to just generate the dot file
+    # TODO allow file type (jpg | png) to be specified
+    # TODO graphviz options (ex: orientation, etc.) -- just pass thru to dot ?
+    # TODO  ex:  options[:orientation] = ENV['ORIENTATION'] if ENV['ORIENTATION']
+
+
     class CLI
+
+      DEFAULT_OUTPUT_PATH = File.absolute_path(File.join(__dir__, '..', '..', '..',))
+      SUCCESS = 0
+
 
       def initialize(options)
         @class_names = Array(options[:class_names])
-        @output_path = options[:output_path] || __dir__
+        @class_files = Array(options[:class_files])
+        @output_path = options[:output_path] || DEFAULT_OUTPUT_PATH
       end
 
+
       def run
-        @class_names.each do |name|
-          # This needs to handle namespace class names
-          klass = Object.const_get(name)
-          edges = ""
-          if initial = klass.aasm.initial_state
-            edges << "initial [shape=point];\n"
-            edges << "initial -> #{initial};\n"
-          end
-          klass.aasm.events.each do |event|
-            event.transitions.each do |transition|
-              edges << "#{transition.from} -> #{transition.to} [ label = \"#{event.name}\" ];\n"
+
+        if required_class_files?(@class_files)
+          @class_names.each do |name|
+
+            # TODO needs to handle namespace class names
+            begin
+              klass = Object.const_get(name)
+              edges = ""
+
+              if (initial = klass.aasm.initial_state)
+                edges << "initial [shape=point];\n"
+                edges << "initial -> #{initial};\n"
+              end
+
+              klass.aasm.events.each do |event|
+                event.transitions.each do |transition|
+                  edges << "#{transition.from} -> #{transition.to} [ label = \"#{event.name}\" ];\n"
+                end
+              end
+
+              # TODO do this based on options (default = yes, do it.  But maybe we just product the dot file)
+              `echo  "#{dot_notation(edges)}" | dot -Tjpg -o #{file_path(name)}` unless edges.empty?
+
+            rescue NameError => e
+              puts "Uh oh! Could not handle a class! #{e.message}"
+              raise e
             end
           end
 
-          `echo  "#{dot_notation(edges)}" | dot -Tjpg -o #{file_path(name)}` unless edges.empty?
         end
+
+        SUCCESS
       end
+
 
       private
 
+      def required_class_files?(files)
+        result = true
+        files.each do |f|
+          begin
+            require(File.absolute_path(f))
+          rescue LoadError => e
+            puts "Uh oh. Had a problem with a class file: #{e.message} "
+            raise e
+          end
+
+        end
+        result
+
+      end
+
+
       def file_path(name)
         File.join(@output_path, "#{name.downcase}.jpg")
+      rescue => e
+        puts "Dagnabbit.  Couldn't create the output file: #{e.message}"
+        raise e
       end
+
 
       def dot_notation(edges)
         <<-DOT
--- test/aasm/graph/cli_test.rb
+++ test/aasm/graph/cli_test.rb
@@ -2,15 +2,88 @@ require_relative "../../test_helper"
 
 class CLITest < Minitest::Test
 
+  TEST_CLASS_FILE_OPTIONS = [File.join('test', 'fixtures', 'job')]
+  TEST_CLASS_OPTIONS = ['Job']
+
+
   def test_graph_file_is_created
-    require 'job'
+
     Dir.mktmpdir do |dir|
+
       file = File.join(dir, 'job.jpg')
+
       AASM::Graph::CLI.new(
-        class_names: ['Job'],
-        output_path: dir
+          class_names: TEST_CLASS_OPTIONS,
+          class_files: TEST_CLASS_FILE_OPTIONS,
+          output_path: dir
       ).run
-      assert File.exists?(file), "File at #{file} doesn't exist"
+
+      assert File.exists?(file), "Output file #{file} wasn't created"
     end
   end
+
+
+  def test_class_files_required
+    result = -1
+    Dir.mktmpdir do |dir|
+
+      result = AASM::Graph::CLI.new(
+          class_names: TEST_CLASS_OPTIONS,
+          class_files: TEST_CLASS_FILE_OPTIONS,
+          output_path: dir
+      ).run
+
+      assert_equal(result, 0, "Could not require #{TEST_CLASS_FILE_OPTIONS.first} file.")
+    end
+  end
+
+
+  def test_bad_class_file
+
+    assert_raises(LoadError) {
+
+      Dir.mktmpdir do |dir|
+        result = AASM::Graph::CLI.new(
+            class_names: TEST_CLASS_OPTIONS,
+            class_files: [File.join('test', 'fixtures', 'DOES-NOT-EXIST')],
+            output_path: dir
+        ).run
+      end
+    }
+  end
+
+
+  def test_class_found
+    result = -1
+    Dir.mktmpdir do |dir|
+
+      result = AASM::Graph::CLI.new(
+          class_names: TEST_CLASS_OPTIONS,
+          class_files: TEST_CLASS_FILE_OPTIONS,
+          output_path: dir
+      ).run
+    end
+
+    assert_equal(result, 0)
+  end
+
+
+  def test_bad_class
+    assert_raises(NameError) {
+      Dir.mktmpdir do |dir|
+
+        result = AASM::Graph::CLI.new(
+            class_names: ['BadClassDoesNotExist'],
+            class_files: TEST_CLASS_FILE_OPTIONS,
+            output_path: dir
+        ).run
+      end
+    }
+  end
+
+
+  # TODO give the output file directory
+
+  # TODO test the information generated in the .dot file is correct
+
 end
